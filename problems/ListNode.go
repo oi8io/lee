ab0a7a0e2a389aeaaad2504ac42570fe0ca1093f
oi8io <vkilo@icloud.com>
2022-04-14 18:35:55 +0800

    ListNode

--- problems/ListNode.go
+++ problems/ListNode.go
@@ -1,1 +1,17 @@
 package problems
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func BuildListNode(in []int) *ListNode {
+	var list *ListNode
+	for i := len(in) - 1; i >= 0; i-- {
+		list = &ListNode{
+			Val:  in[i],
+			Next: list,
+		}
+	}
+	return list
+}
