8749af882735555690f8378420da7cea28c7a4c3
oi8io <vkilo@icloud.com>
2022-04-23 23:33:24 +0800

    add 191

---
+++ problems/191.位1的个数_test.go
@@ -0,0 +1,24 @@
+package problems
+
+import "testing"
+
+func Test_hammingWeight(t *testing.T) {
+	type args struct {
+		num uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"", args{1234121}, 10},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hammingWeight(tt.args.num); got != tt.want {
+				t.Errorf("hammingWeight() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
