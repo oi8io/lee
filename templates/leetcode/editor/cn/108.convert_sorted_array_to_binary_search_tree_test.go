98150f13aa8eaa5eb187b2e9054264247f49dd27
oi8io <vkilo@icloud.com>
2022-05-12 22:15:23 +0800

    add 108

---
+++ templates/leetcode/editor/cn/108.convert_sorted_array_to_binary_search_tree_test.go
@@ -0,0 +1,27 @@
+package problems
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_sortedArrayToBST(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+		{"", args{[]int{-10, -3, 0, 5, 9}}, nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortedArrayToBST(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortedArrayToBST() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
