ff3314ed0926d0662c07d83e039606c9f8773ed6
oi8io <vkilo@icloud.com>
2022-05-15 00:04:14 +0800

    add double weekly

---
+++ templates/leetcode/editor/cn/5299.go
@@ -0,0 +1,21 @@
+package problems
+
+func divisorSubstrings(num int, k int) int {
+	var answers = 0
+	var cal = 1
+	for i := 1; i <= k; i++ {
+		cal *= 10
+	}
+	var x = num
+	for {
+		if x < cal/10 {
+			break
+		}
+		cur := x % cal
+		if cur != 0 && num%cur == 0 {
+			answers++
+		}
+		x = x / 10
+	}
+	return answers
+}
