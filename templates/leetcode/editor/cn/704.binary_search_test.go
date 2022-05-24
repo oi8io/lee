c4964f9c3feb66e9df786768c0cbc0d29f273b59
oi8io <vkilo@icloud.com>
2022-05-24 08:47:42 +0800

    add bin

---
+++ templates/leetcode/editor/cn/704.binary_search_test.go
@@ -0,0 +1,26 @@
+package problems
+
+import "testing"
+
+func Test_search(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{args: args{[]int{-1, 0, 3, 5, 9, 12}, 2}, want: -1},
+		{args: args{[]int{-1, 0, 3, 5, 9, 12}, 9}, want: 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := search(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("search() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
